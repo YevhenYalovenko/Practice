0a678acc2db1d7a39bea8a0ba438e9e930361cab
YevhenYalovenko <jenyaya0007@gmail.com>
2022-07-09 17:38:31 +0300

    personalMovieDB

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict';
 
 const numberOfFilms = prompt('Сколько фильмов Вы уже смотрели?', '');
+
+const personalMovieDB = {
+    count: console.log (numberOfFilms),
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log (personalMovieDB);
