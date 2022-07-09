34f5c269d02bb5b5d4b5b7ec1fdd597b8d9aabad
YevhenYalovenko <jenyaya0007@gmail.com>
2022-07-09 18:58:53 +0300

    Film:Rating

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов Вы уже смотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов Вы уже смотрели?', '');
 
 const personalMovieDB = {
-    count: console.log (numberOfFilms),
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const movieA = prompt('Один из последних просмотренных фильмов?', '');
+const ratingA = prompt('На сколько оцените его?', '');  
+const movieB = prompt('Один из последних просмотренных фильмов?', '');
+const ratingB = prompt('На сколько оцените его?', '');  
+
+
+personalMovieDB.movies[movieA] = ratingA;
+personalMovieDB.movies[movieB] = ratingB;
+
 console.log (personalMovieDB);
