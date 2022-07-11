3564f4e04a9e8de8937707d92825548a56072721
YevhenYalovenko <jenyaya0007@gmail.com>
2022-07-11 23:50:46 +0300

    Three types of Cycless

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ const personalMovieDB = {
     privat: false
 };
 
-FirstStep:for(let i=0; i<2; i++) {
+if (personalMovieDB.count < 10) {
+    alert ('На жаль, Ви подивилися мало фільмів'); 
+} else if (personalMovieDB.count < 30) {
+    alert ('Ви класичний глядач');
+} else if (personalMovieDB.count >= 30) {
+    alert ('Ви справжній кіноман');
+} else {
+    console.log ('Виникла помилка');
+}
+
+// First method
+for(let i=0; i<2; i++) {
     const movie = prompt('Один из последних просмотренных фильмов?', '');
     const rating = +prompt('На сколько оцените его?', '');
 
@@ -23,3 +34,38 @@ FirstStep:for(let i=0; i<2; i++) {
    }
 console.log(personalMovieDB);
 
+
+// Second method    
+//     let a = 0;
+
+//     while(a<2) {
+//         let movie = prompt('Один из последних просмотренных фильмов?', '');
+//         let rating = +prompt('На сколько оцените его?', '');
+//         if (movie !== null && movie !== '' && rating !== null && rating !== '' && movie.length < 50) {
+//             personalMovieDB.movies[movie] = rating;
+//             a++;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             a--;
+//         }
+//     } 
+
+// console.log(personalMovieDB);
+
+// Third method
+// let a = 0;
+// do {
+//     let movie = prompt('Один из последних просмотренных фильмов?', '');
+//     let rating = +prompt('На сколько оцените его?', '');
+//     if (movie !== null && movie !== '' && rating !== null && rating !== '' &&  movie.length < 50) {
+//         personalMovieDB.movies[movie] = rating;
+//         a++;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         a--;
+//     }
+// }
+// while(a<2);
+// console.log(personalMovieDB);
